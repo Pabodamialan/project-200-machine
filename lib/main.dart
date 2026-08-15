14208fd1a40a260991262781d0e8cdfffc230f23
Paboda Milan <pabodamilan@Pabodas-MacBook-Air.local>
2026-08-15 05:53:45 +0530

    Add custom domain branding: NODA Civimech Engineering title, favicon, PWA manifest, and MaterialApp title fix

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Project 200 Machine',
+      title: 'NODA Civimech Engineering (PVT) Ltd.',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
       home: AuthGate(key: UniqueKey()),
